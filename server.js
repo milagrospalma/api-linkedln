2c5641699bc1add2054602c471dd723ae5f4e39f
lucerohospina <lucerojhp@gmail.com>
2018-02-16 09:13:50 -0500

    gitignore is working properly

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = app.listen(3000, onServer);
 
 function onServer() {
-  console.log('Servidor encendido.');
+  console.log('server is on');
 }
 
 app.use(express.static('public'));
